1171a84fa1c10ff2596845569ab70b97790a77f7
Natãn Schneider <natanschneider@protonmail.com>
2024-11-10 15:53:34 -0300

    *upload* initial files

---
+++ src/app/layout.tsx
@@ -0,0 +1,21 @@
+import type { Metadata } from "next";
+import "./globals.css";
+
+export const metadata: Metadata = {
+  title: "Natãn Schneider",
+  description: "Full stack software developer",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="pt-BR">
+      <body>
+        {children}
+      </body>
+    </html>
+  );
+}

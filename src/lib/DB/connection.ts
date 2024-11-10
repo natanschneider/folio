af774c32ac0b0501bfdf4bc395ea7d42a4ef431d
Natãn Schneider <natanschneider@protonmail.com>
2024-11-10 17:59:03 -0300

    *fix* indentation

--- src/lib/DB/connection.ts
+++ src/lib/DB/connection.ts
@@ -3,7 +3,7 @@ import { Pool } from 'pg';
 export const pool = new Pool({
 	user: process.env.PGUSER,
 	host: process.env.PGHOST,
-   database: process.env.PGDATABASE,
-   password: process.env.PGPASSWORD,
-   port: process.env.PGPORT
+	database: process.env.PGDATABASE,
+	password: process.env.PGPASSWORD,
+	port: process.env.PGPORT
 })

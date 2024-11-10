242c3f9d724eb5140eccdaf1977fe8f314a2b717
Natãn Schneider <natanschneider@protonmail.com>
2024-11-10 18:46:43 -0300

    *feat* fetch initial data

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,27 @@
-export default function Home() {
+import { basicQuery } from '@/lib/DB/connection'
+
+interface timeline {
+  id: number,
+  name: string,
+  description: string,
+  year: string,
+  duration: string,
+  status: boolean
+}
+
+interface projects {
+  id: number,
+  nome: string,
+  img_url: string,
+  url: string,
+  repo: string,
+  status: boolean,
+  stack: string[]
+}
+
+export default async function Home() {
+  const timeline:timeline = await basicQuery(`SELECT * FROM TIMELINE WHERE STATUS = TRUE`)
+  const projects = await basicQuery(`SELECT * FROM PROJECTS WHERE STATUS = TRUE`)
   return (
       <div></div>
   );
